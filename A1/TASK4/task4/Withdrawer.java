1e7080dbd677cdae7973f645b0fbc0341e2c18b9
ziadyrb <ziadyrb@outlook.com>
2017-02-27 22:11:25 -0500

    Add files via upload

---
+++ A1/TASK4/task4/Withdrawer.java
@@ -0,0 +1,37 @@
+package task4;
+
+/**
+ * Class Withdrawer
+ * Implements Withdrawer thread class.
+ *
+ * @author Malek Barhoush, mbarhoush@hotmail.com;
+ * 
+ *
+ * $Revision: 1.0 $
+ * $Last Revision Date: 2011/09/06
+ */
+
+public class Withdrawer extends Thread {
+	private Account account ;
+	public Withdrawer(Account account){
+		this.account = account;
+	}
+	
+	public void run(){
+	
+		// Withdraw 10 CAD into instance variable account
+		for (int i=0;i<10000000;i++)
+		{
+			account.withdraw(10);
+			/*
+			try {
+				sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/
+		}
+		
+	}
+
+}
